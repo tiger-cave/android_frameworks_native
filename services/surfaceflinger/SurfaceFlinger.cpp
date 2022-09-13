259a06e93c1929b6b0b88d9244af9d006b0001d9
Khalvat-M <mmba313@gmail.com>
2022-09-13 11:56:41 +0430

    SurfaceFlinger: Don't cleanup resources from previous frame on display
    we have this again ha ha
    
    Change-Id: I3ffe5247efdf5053a8ae5b9b43d3cdff1272a1fe
    (cherry picked from commit 2456bab333f9fdb4e8d1ced7b88b835ec1ba756d)
    (cherry picked from commit 5345c4ea75e58a209adb4509fb40153d26ca142d)

--- services/surfaceflinger/SurfaceFlinger.cpp
+++ services/surfaceflinger/SurfaceFlinger.cpp
@@ -3198,7 +3198,9 @@ void SurfaceFlinger::onCompositionPresented(PhysicalDisplayId pacesetterId,
     }
 
     if (hasPacesetterDisplay && !pacesetterDisplay->isPoweredOn()) {
+#ifndef DISABLE_POSTRENDER_CLEANUP
         getRenderEngine().cleanupPostRender();
+#endif
         return;
     }
 
