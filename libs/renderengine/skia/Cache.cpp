afe62b8b9711924f8e3d21d1e6590b1c79fc863f
Yaroslav Zviezda <acroreiser@gmail.com>
2024-03-31 21:53:35 +0300

    Revert "RenderEngine, SF: gate HDR priming behind property"
    This reverts commit b9b5b706d449212094fd020dfd30509234a8c0b4.
    
    Change-Id: I749d945114f547fdb7152f3360026040b0f728dc

--- libs/renderengine/skia/Cache.cpp
+++ libs/renderengine/skia/Cache.cpp
@@ -21,7 +21,6 @@
 #include "ShaderCache.h"
 #include "SkiaRenderEngine.h"
 #include "android-base/unique_fd.h"
-#include "cutils/properties.h"
 #include "renderengine/DisplaySettings.h"
 #include "renderengine/LayerSettings.h"
 #include "renderengine/impl/ExternalTexture.h"
