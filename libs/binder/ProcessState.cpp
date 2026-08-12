80fa89c80acf6c0f695492f2dc39f5226a3b14c7
GuaiYiHu <guaiyihu@foxmail.com>
2026-08-13 00:41:04 +0800

    libbinder: make threadpool shrinking non-fatal
    Adapt the legacy behavior to Android 15 by retaining a warning when a running threadpool is reduced.
    
    Change-Id: Ifd15f25d44200d1bb8cea3e288afcb0eb56f9aa8

--- libs/binder/ProcessState.cpp
+++ libs/binder/ProcessState.cpp
@@ -449,8 +449,10 @@ void ProcessState::spawnPooledThread(bool isMain)
 }
 
 status_t ProcessState::setThreadPoolMaxThreadCount(size_t maxThreads) {
-    LOG_ALWAYS_FATAL_IF(mThreadPoolStarted && maxThreads < mMaxThreads,
-           "Binder threadpool cannot be shrunk after starting");
+    if (mThreadPoolStarted && maxThreads < mMaxThreads) {
+        ALOGW("Shrinking Binder threadpool after it has started: %zu -> %zu", mMaxThreads,
+              maxThreads);
+    }
     status_t result = NO_ERROR;
     if (ioctl(mDriverFD, BINDER_SET_MAX_THREADS, &maxThreads) != -1) {
         mMaxThreads = maxThreads;
